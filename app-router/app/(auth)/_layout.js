e2222e8d1b489ad61f8a55360b7241142f41fbab
Gustavo Hackime Costa <gustavohackime@gmail.com>
2026-05-05 20:33:20 -0300

    feat: Adicionado logomarca FIAP ao cabeçalho

--- app-router/app/(auth)/_layout.js
+++ app-router/app/(auth)/_layout.js
@@ -1,8 +1,13 @@
 import { Stack } from "expo-router";
+import { LogoTitle } from '../../utils/headerLogo';
 
 export default function Layout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack screenOptions={{ 
+      headerTitle: () => <LogoTitle />,
+      headerTitleAlign: 'center',
+      headerStyle: { backgroundColor: '#F3F4F6' }, 
+    }}>
       <Stack.Screen name="login" />
       <Stack.Screen name="cadastro" />
     </Stack>
